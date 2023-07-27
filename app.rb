36fbb91f2984e9b0e344999ab8ce6e6405eb769b
Jrmcuervo <jrmcuervom@gmail.com>
2023-07-27 11:03:25 -0600

    Fix linters

--- app.rb
+++ app.rb
@@ -6,75 +6,76 @@ require_relative 'student'
 require_relative 'teacher'
 
 class App
-    def initialize
-      @books = []
-      @people = []
-      @rentals = []
-      @classrooms = []
-    end
-  
-    def list_all_books
-      @books.each do |book|
-        puts "Title: #{book.title}, Author: #{book.author}"
-      end
-    end
-  
-    def list_all_people
-      @people.each do |person|
-        puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
-      end
-    end
-  
-    def create_person(type, age, name, parent_permission, classroom_name = nil, specialization = nil)
-        person = case type.downcase
-                 when 'student'
-                   classroom = @classrooms.find { |c| c.label == classroom_name }
-                   if classroom.nil?
-                     classroom = Classroom.new(classroom_name)
-                     @classrooms << classroom
-                   end
-                   Student.new(age, classroom, name: name, parent_permission: parent_permission)
-                 when 'teacher'
-                   Teacher.new(age, specialization, name: name, parent_permission: parent_permission)
-                 else
-                   puts 'Invalid person type'
-                   return
-                 end
-    
-        @people << person
-        puts "#{type} created successfully"
-      end
-  
-    def create_book(title, author)
-      book = Book.new(title, author)
-      @books << book
-      puts "Book created successfully"
-    end
-  
-    def create_rental(date, book_title, person_id)
-      book = @books.find { |book| book.title == book_title }
-      person = @people.find { |person| person.id == person_id }
-  
-      if book.nil?
-        puts "No book with title: #{book_title}"
-        return
-      end
-  
-      if person.nil?
-        puts "No person with ID: #{person_id}"
-        return
-      end
-  
-      rental = Rental.new(date, book, person)
-      @rentals << rental
-      puts "Rental created successfully"
-    end
-  
-    def list_all_rentals_for_person_id(person_id)
-      rentals_for_person = @rentals.select { |rental| rental.person.id == person_id }
-      rentals_for_person.each do |rental|
-        puts "Date: #{rental.date}, Book: #{rental.book.title}"
-      end
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+    @classrooms = []
+  end
+
+  def list_all_books
+    @books.each do |book|
+      puts "Title: #{book.title}, Author: #{book.author}"
     end
   end
-  
+
+  def list_all_people
+    @people.each do |person|
+      puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+    end
+  end
+
+  def create_person(attrs = {})
+    attrs[:classroom] = find_or_create_classroom(attrs[:classroom_name]) if attrs[:type] == 'student'
+    person = create_student_or_teacher(attrs)
+
+    @people << person
+    puts "#{attrs[:type]} created successfully"
+  end
+
+  def create_student_or_teacher(attrs = {})
+    case attrs[:type]
+    when 'student'
+      Student.new(attrs[:age], attrs[:classroom], name: attrs[:name], parent_permission: attrs[:parent_permission])
+    when 'teacher'
+      Teacher.new(attrs[:age], attrs[:specialization], name: attrs[:name], parent_permission: attrs[:parent_permission])
+    else
+      puts 'Invalid person type'
+      nil
+    end
+  end
+
+  def find_or_create_classroom(classroom_name)
+    classroom = @classrooms.find { |c| c.label == classroom_name }
+    return classroom unless classroom.nil?
+
+    new_classroom = Classroom.new(classroom_name)
+    @classrooms << new_classroom
+    new_classroom
+  end
+
+  def create_book(title, author)
+    book = Book.new(title, author)
+    @books << book
+    puts 'Book created successfully'
+  end
+
+  def create_rental(date, book_title, person_id)
+    book_to_rent = @books.find { |b| b.title == book_title }
+    person_to_rent = @people.find { |p| p.id == person_id }
+
+    return puts "No book with title: #{book_title}" if book_to_rent.nil?
+    return puts "No person with ID: #{person_id}" if person_to_rent.nil?
+
+    rental = Rental.new(date, book_to_rent, person_to_rent)
+    @rentals << rental
+    puts 'Rental created successfully'
+  end
+
+  def list_all_rentals_for_person_id(person_id)
+    rentals_for_person = @rentals.select { |rental| rental.person.id == person_id }
+    rentals_for_person.each do |rental|
+      puts "Date: #{rental.date}, Book: #{rental.book.title}"
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -4,15 +4,7 @@ def main
   app = App.new
 
   loop do
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-
+    display_menu
     option = gets.chomp
 
     case option
@@ -21,50 +13,13 @@ def main
     when '2'
       app.list_all_people
     when '3'
-      puts 'Do you want to create a student (1) or a teacher (2)?'
-      person_type = gets.chomp == '1' ? 'student' : 'teacher'
-
-      puts 'Age?'
-      age = gets.chomp.to_i
-
-      puts 'Name?'
-      name = gets.chomp
-
-      puts 'Parent permission? [Y/N]'
-      parent_permission = gets.chomp.downcase == 'y'
-
-      if person_type == 'student'
-        puts 'Classroom?'
-        classroom = gets.chomp
-        app.create_person(person_type, age, name, parent_permission, classroom)
-      else
-        puts 'Specialization?'
-        specialization = gets.chomp
-        app.create_person(person_type, age, name, parent_permission, nil, specialization)
-      end
+      create_person(app)
     when '4'
-      puts 'Title?'
-      title = gets.chomp
-
-      puts 'Author?'
-      author = gets.chomp
-
-      app.create_book(title, author)
+      create_book(app)
     when '5'
-      puts 'Date?'
-      date = gets.chomp
-
-      puts 'Book title?'
-      book_title = gets.chomp
-
-      puts 'Person id?'
-      person_id = gets.chomp.to_i
-
-      app.create_rental(date, book_title, person_id)
+      create_rental(app)
     when '6'
-      puts 'Person id?'
-      person_id = gets.chomp.to_i
-      app.list_all_rentals_for_person_id(person_id)
+      list_rentals_for_person(app)
     when '7'
       puts 'Exit the app'
       break
@@ -72,6 +27,72 @@ def main
       puts 'Invalid option'
     end
   end
+end
+
+def display_menu
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
+def create_person(app)
+  puts 'Do you want to create a student (1) or a teacher (2)?'
+  person_type = gets.chomp == '1' ? 'student' : 'teacher'
+
+  puts 'Age?'
+  age = gets.chomp.to_i
+
+  puts 'Name?'
+  name = gets.chomp
+
+  puts 'Parent permission? [Y/N]'
+  parent_permission = gets.chomp.downcase == 'y'
+
+  if person_type == 'student'
+    puts 'Classroom?'
+    classroom_name = gets.chomp
+    app.create_person(type: person_type, age: age, name: name, parent_permission: parent_permission,
+                      classroom_name: classroom_name)
+  else
+    puts 'Specialization?'
+    specialization = gets.chomp
+    app.create_person(type: person_type, age: age, name: name, parent_permission: parent_permission,
+                      specialization: specialization)
+  end
+end
+
+def create_book(app)
+  puts 'Title?'
+  title = gets.chomp
+
+  puts 'Author?'
+  author = gets.chomp
+
+  app.create_book(title, author)
+end
+
+def create_rental(app)
+  puts 'Date?'
+  date = gets.chomp
+
+  puts 'Book title?'
+  book_title = gets.chomp
+
+  puts 'Person id?'
+  person_id = gets.chomp.to_i
+
+  app.create_rental(date, book_title, person_id)
+end
+
+def list_rentals_for_person(app)
+  puts 'Person id?'
+  person_id = gets.chomp.to_i
+  app.list_all_rentals_for_person_id(person_id)
 end
 
 main
